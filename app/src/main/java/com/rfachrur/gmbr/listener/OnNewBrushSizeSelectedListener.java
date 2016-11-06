cca91284f0889c1a82202bf6edb9f4a9cdb84f20
Mohamad Fachrur Ridwan <ridwan.fachrur@gmail.com>
2016-11-06 18:14:59 +0700

    phase 1, still on going

---
+++ app/src/main/java/com/rfachrur/gmbr/listener/OnNewBrushSizeSelectedListener.java
@@ -0,0 +1,12 @@
+package com.rfachrur.gmbr.listener;
+
+/**
+ * Created by rfachrur on 11/6/16.
+ *
+ */
+
+public interface OnNewBrushSizeSelectedListener {
+
+    void OnNewBrushSizeSelected(float newBrushSize);
+
+}
